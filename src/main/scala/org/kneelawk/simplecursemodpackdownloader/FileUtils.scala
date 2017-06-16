ab5a1813a61529e7940eb9d828df2d29284b5e04
Kneelawk <jedidiah.pommert@gmail.com>
2017-06-16 15:50:12 -0700

    Fixed file copying

--- src/main/scala/org/kneelawk/simplecursemodpackdownloader/FileUtils.scala
+++ src/main/scala/org/kneelawk/simplecursemodpackdownloader/FileUtils.scala
@@ -6,6 +6,8 @@ import java.io.FileOutputStream
 import java.nio.file.Files
 import java.util.zip.ZipInputStream
 import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+import java.nio.file.LinkOption
 
 object FileUtils {
   def unzipZip(zip: File): File = {
@@ -29,14 +31,26 @@ object FileUtils {
     return tempDir
   }
 
-  def copyFile(from: File, to: File) {
-    Files.copy(from.toPath(), to.toPath())
+  def copyFile(from: File, to: File, createParents: Boolean) {
+    if (createParents && !to.getParentFile.exists())
+      to.getParentFile.mkdirs()
+    Files.copy(from.toPath(), to.toPath(), StandardCopyOption.REPLACE_EXISTING, LinkOption.NOFOLLOW_LINKS)
   }
 
   def copyDir(from: File, to: File) {
     val fromPath = from.toPath()
     val toPath = to.toPath()
-    Files.walk(fromPath).filter((p: Path) => Files.isDirectory(p))
-      .forEach((p: Path) => Files.copy(p, toPath.resolve(fromPath.relativize(p))))
+    Files.walk(fromPath)
+      .forEach((p: Path) => {
+        val toIndividual = toPath.resolve(fromPath.relativize(p))
+        if (Files.isDirectory(p, LinkOption.NOFOLLOW_LINKS)) {
+          if (!Files.exists(toIndividual))
+            Files.createDirectories(toIndividual)
+        } else {
+          if (!Files.exists(toIndividual.getParent))
+            Files.createDirectories(toIndividual.getParent)
+          Files.copy(p, toIndividual, StandardCopyOption.REPLACE_EXISTING, LinkOption.NOFOLLOW_LINKS)
+        }
+      })
   }
 }
