fa6ef6de1123a212e8e63f163f30efdd5a715727
Kneelawk <jedidiah.pommert@gmail.com>
2017-06-16 17:22:53 -0700

    Partially finish ModEngine

--- src/main/scala/org/kneelawk/simplecursemodpackdownloader/ModEngine.scala
+++ src/main/scala/org/kneelawk/simplecursemodpackdownloader/ModEngine.scala
@@ -9,6 +9,8 @@ import scala.util.Success
 import dispatch.Defaults.executor
 import dispatch.Http
 import dispatch.StatusCode
+import java.net.URL
+import java.net.URI
 
 /*
  * This file is where the magic happens.
@@ -16,21 +18,46 @@ import dispatch.StatusCode
 
 class ModpackEngine(client: Http, authToken: String, modpack: ModpackManifest,
     modsDir: File, listener: ModpackProgressListener) {
-  import ModEngineState._
+  import EngineState._
 
   def start {
     /*
      * Note: This system doesn't regulate the number of concurrent downloads.
      * This system will likely need to be modified.
      */
-    var engines = modpack.files.map(f =>
-      new ModEngine(client, authToken, modpack.minecraft.version, f.projectId,
-        f.fileId, modsDir, listener.createModProgressListener(f.projectId, f.fileId)))
-    engines.foreach(_.start)
+    def engineLoop: EngineState.Value = {
+      val numMods = modpack.files.size
+      var engines = modpack.files.map(f =>
+        new ModEngine(client, authToken, modpack.minecraft.version, f.projectId,
+          f.fileId, modsDir, listener.createModProgressListener(f.projectId, f.fileId)))
+      engines.foreach(_.start)
 
-    while (engines.size > 0) {
-      engines = engines.filter(e => e.state == Running || e.state == Crashed)
-      engines.filter(_.state == Crashed).foreach(_.start)
+      var numEngines = engines.size
+      while (engines.size > 0) {
+        engines = engines.filter(e => e.state != Finished)
+        engines.filter(_.state == Crashed).foreach(_.start)
+        if (engines.count(_.state == Abort) > 0) {
+          return Abort
+        }
+        if (engines.count(_.state == Dead) > 0) {
+          return Dead
+        }
+        if (engines.size < numEngines && engines.size != 0) {
+          numEngines = engines.size
+          listener.onOverallProgress(numMods - numEngines, numMods)
+        }
+        Thread.sleep(200)
+      }
+      return Finished
+    }
+
+    val result = engineLoop
+    if (result == Abort) {
+      listener.onAbort
+    } else if (result == Dead) {
+      listener.onDeath
+    } else {
+      listener.onAllModsComplete
     }
   }
 }
@@ -40,23 +67,27 @@ class ModpackEngine(client: Http, authToken: String, modpack: ModpackManifest,
  */
 class ModEngine(client: Http, authToken: String, minecraftVersion: String,
     projectId: Int, fileId: Int, modsDir: File, listener: ModProgressListener) {
-  import ModEngineState._
+  import EngineState._
 
   @volatile var state = NotStarted
 
   var error: Throwable = null
 
   def start {
-    state = Running
     if (state != Running) {
+      state = Running
       listener.onBeginResolvingMod
       val futMod = CurseUtils.getExistingAddonFile(client, authToken, projectId, fileId, minecraftVersion)
       futMod.onComplete {
         case Success(mod) => {
           listener.onModResolved(mod)
+          
+          // copied from https://stackoverflow.com/a/8962869/1687581
+          val url = new URL(mod.downloadUrl)
+          val sanitaryUri = new URI(url.getProtocol(), url.getHost(), url.getPath(), url.getRef());
 
           val outFile = new File(modsDir, mod.downloadUrl.replaceAll("^.*\\/", ""))
-          val download = new Download(outFile, mod.downloadUrl)
+          val download = new Download(outFile, sanitaryUri.toASCIIString())
             .onDownloadStarted(d => listener.onBeginModDownload)
             .onDownloadProgress(p => listener.onModDownloadProgress(p.downloaded, p.maxSize))
             .onDownloadError(err => listener.onError(err))
@@ -64,25 +95,25 @@ class ModEngine(client: Http, authToken: String, minecraftVersion: String,
 
           download.onComplete {
             case Success(f) => {
-              listener.onCompletedModDownload
               state = Finished
+              listener.onCompletedModDownload
             }
             case Failure(err) => {
               err match {
                 case t: ExecutionException if t.getCause.isInstanceOf[StatusCode] => {
-                  listener.onDeath(err)
                   error = err
                   state = Dead
+                  listener.onDeath(err)
                 }
                 case StatusCode(_) => {
-                  listener.onDeath(err)
                   error = err
                   state = Dead
+                  listener.onDeath(err)
                 }
                 case _ => {
-                  listener.onCrash(err)
                   error = err
                   state = Crashed
+                  listener.onCrash(err)
                 }
               }
             }
@@ -91,24 +122,24 @@ class ModEngine(client: Http, authToken: String, minecraftVersion: String,
         case Failure(err) => {
           err match {
             case NoFileForMinecraftVersionException(_) => {
-              listener.onDeath(err)
               error = err
               state = Dead
+              listener.onDeath(err)
             }
             case t: ExecutionException if t.getCause.isInstanceOf[StatusCode] => {
-              listener.onDeath(err)
               error = err
               state = Dead
+              listener.onDeath(err)
             }
             case StatusCode(_) => {
-              listener.onDeath(err)
               error = err
               state = Dead
+              listener.onDeath(err)
             }
             case _ => {
-              listener.onCrash(err)
               error = err
               state = Crashed
+              listener.onCrash(err)
             }
           }
         }
@@ -124,6 +155,6 @@ class ModEngine(client: Http, authToken: String, minecraftVersion: String,
  * Death - means that a fatal error has occurred and anything relying on this process cannot continue.
  */
 
-object ModEngineState extends Enumeration {
+object EngineState extends Enumeration {
   val NotStarted, Running, Finished, Crashed, Dead, Abort = Value
 }
--- src/main/scala/org/kneelawk/simplecursemodpackdownloader/cli/CLIDownloader.scala
+++ src/main/scala/org/kneelawk/simplecursemodpackdownloader/cli/CLIDownloader.scala
@@ -17,8 +17,13 @@ import com.ning.http.client.AsyncHttpClientConfig
 
 import dispatch.Defaults.executor
 import dispatch.Http
+import org.kneelawk.simplecursemodpackdownloader.ModProgressListener
+import org.kneelawk.simplecursemodpackdownloader.ModFile
+import org.kneelawk.simplecursemodpackdownloader.ModpackProgressListener
+import org.kneelawk.simplecursemodpackdownloader.ModpackEngine
 
 object CLIDownloader {
+  var client: Http = null
   def apply(args: Array[String]) {
     if (args.length < 3) {
       println("cli <input-zip> <output-dir>")
@@ -36,13 +41,13 @@ object CLIDownloader {
     val outDir = new File(args(2))
 
     println("Logging in...")
-    
+
     val config = new AsyncHttpClientConfig.Builder()
       .setUserAgent("CurseModpackDownloader/0.0.1")
       .setRequestTimeout(-1)
       .setFollowRedirect(true)
       .build()
-    val client = new Http(new AsyncHttpClient(config))
+    client = new Http(new AsyncHttpClient(config))
     val futUser = CurseUtils.auth(client, username, password)
     futUser.onComplete {
       case Success(user) => {
@@ -57,24 +62,93 @@ object CLIDownloader {
   }
 
   def beginExtraction(user: UserData, modpackFile: File, outDir: File) {
-    outDir.mkdirs()
-    
-    println()
-    println("Loading modpack...")
-    val modpack = Modpack.load(modpackFile)
-    println("Modpack name: " + modpack.manifest.name)
-    println("Modpack version: " + modpack.manifest.version)
-    println("Modpack author: " + modpack.manifest.author)
-    println("Minecraft version: " + modpack.manifest.minecraft.version)
-    println()
-    println("Mod loaders:")
-    modpack.manifest.minecraft.modLoaders.foreach { l =>
-      println('\t' + l.id + (if (l.primary) " - primary" else ""))
+    try {
+      outDir.mkdirs()
+
+      println()
+      println("Loading modpack...")
+      val modpack = Modpack.load(modpackFile)
+      println("Modpack name: " + modpack.manifest.name)
+      println("Modpack version: " + modpack.manifest.version)
+      println("Modpack author: " + modpack.manifest.author)
+      println("Minecraft version: " + modpack.manifest.minecraft.version)
+      println()
+      println("Mod loaders:")
+      modpack.manifest.minecraft.modLoaders.foreach { l =>
+        println('\t' + l.id + (if (l.primary) " - primary" else ""))
+      }
+      println()
+
+      println("Copying overrides...")
+      FileUtils.copyFile(modpack.manifestFile, new File(outDir, "manifest.json"), true)
+      FileUtils.copyDir(modpack.overridesDir, outDir)
+
+      println("Downloading files...")
+      object ModpackListener extends ModpackProgressListener {
+        def createModProgressListener(projectId: Int, fileId: Int) = new ModListener(projectId, fileId)
+        def onOverallProgress(current: Long, max: Long) = println(s"## Completed Mod download: $current / $max")
+        def onAllModsComplete {
+          println("## Mod Downloads Complete")
+          terminate
+        }
+        def onDeath {
+          println("Fatal error")
+          terminate
+        }
+        def onAbort {
+          println("Download aborted")
+          terminate
+        }
+      }
+
+      val engine = new ModpackEngine(client, user.authToken, modpack.manifest, new File(outDir, "mods"), ModpackListener)
+      engine.start
+    } catch {
+      case _: Exception => terminate
     }
-    println()
-    
-    println("Copying overrides...")
-    FileUtils.copyFile(modpack.manifestFile, new File(outDir, "manifest.json"))
-    FileUtils.copyDir(modpack.overridesDir, new File(outDir, modpack.overridesName))
+  }
+
+  class ModListener(projectId: Int, fileId: Int) extends ModProgressListener {
+    var m: ModFile = null
+    var lastProgress = 0l
+
+    def onBeginResolvingMod = println(s"Starting resolve: $projectId")
+
+    def onModResolved(mod: ModFile) = m = mod
+
+    def onBeginModDownload = println(s"Starting download: ${m.fileName}")
+
+    def onModDownloadProgress(current: Long, max: Long) {
+      if (current > lastProgress + 1000) {
+        println(s"Downloading ${m.fileName} ${current * 100 / max}%")
+      }
+    }
+
+    def onCompletedModDownload {
+      println(s"Finished downlading ${m.fileName}")
+    }
+
+    def onError(t: Throwable) {
+      println(s"Error while downloading ${if (m != null) m.fileName else projectId}:")
+      println(t.toString())
+    }
+
+    def onCrash(t: Throwable) {
+      println(s"Recoverable error while downloading ${if (m != null) m.fileName else projectId}:")
+      println(t.toString())
+    }
+
+    def onDeath(t: Throwable) {
+      println(s"Fatal error while downlading ${if (m != null) m.fileName else projectId}:")
+      println(t.toString())
+    }
+
+    def onAbort {
+      println(s"Aborted ${if (m != null) m.fileName else projectId}")
+    }
+  }
+
+  def terminate {
+    client.shutdown()
   }
 }
