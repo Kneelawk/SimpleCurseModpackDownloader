80ac53665fb5ffc0af0a9ba975e709f308c3d467
Kneelawk <jedidiah.pommert@gmail.com>
2017-06-25 03:15:46 -0700

    Remove an empty body

--- src/main/scala/org/kneelawk/simplecursemodpackdownloader/io/InvalidModFileException.scala
+++ src/main/scala/org/kneelawk/simplecursemodpackdownloader/io/InvalidModFileException.scala
@@ -2,6 +2,4 @@ package org.kneelawk.simplecursemodpackdownloader.io
 
 import java.io.IOException
 
-class InvalidModFileException(message: String) extends IOException(message) {
-  
-}
+class InvalidModFileException(message: String) extends IOException(message)
