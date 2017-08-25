ee8e73d0bff24b9f4bae67adb8d11dd4db2f9b58
Kneelawk <jedidiah.pommert@gmail.com>
2017-08-24 21:11:35 -0700

    Comment on UntypedTaskEventBus

--- src/main/scala/org/kneelawk/simplecursemodpackdownloader/UntypedTaskEventBus.scala
+++ src/main/scala/org/kneelawk/simplecursemodpackdownloader/UntypedTaskEventBus.scala
@@ -15,14 +15,33 @@ import scala.reflect.ClassTag
  * You know what possible events can be fired.
  * 
  * Cons of type-specified event busses:
- * Seperate classes for each set of events.
+ * Separate classes for each set of events.
  * Requires macros for non-tedious implementation.
+ * 
+ * Other possible idea: Just have subclasses of this class that implement a
+ * method checking if an event type is valid. An unsupported something exception
+ * would be thrown for invalid event types.
+ * 
+ * Event polymorphism?
+ * 
+ * Another idea: Make abstract. Subclasses will pass a list of types of events that they will throw.
+ * (or more correctly, a list of the lowest level event types they will let listeners register for,
+ * and the highest level event types they can throw. (These are the class keys for listener holding maps.
+ * (so no multimaps)))
+ * Registering a listener will add it to all the sets for events its event type superclasses.
+ * If a registering listener's event type isn't a superclass of any of the specified events, throw some exception.
+ * You can actually throw events more specific than the ones you specified, just noone will be able to listen
+ * for just those events. You will not be able to throw any events more generic than the ones you specified.
+ * 
+ * Use a base class for all events?
+ * 
+ * I don't think this would need a base class for all events. Trait events???
  */
 class UntypedTaskEventBus(task: UntypedTaskEventBus => Unit) {
   val listeners = new HashMap[Class[_], Set[EventListener[_]]] with MultiMap[Class[_], EventListener[_]]
 
-  def onEvent[EventType](listener: EventType => Unit)(implicit ct: ClassTag[EventType]) {
-    listeners.addBinding(ct.runtimeClass, new EventListener[EventType](listener))
+  def onEvent[EventType: ClassTag](listener: EventType => Unit) {
+    listeners.addBinding(implicitly[ClassTag[EventType]].runtimeClass, new EventListener[EventType](listener))
   }
 
   def sendEvent(event: AnyRef) {
