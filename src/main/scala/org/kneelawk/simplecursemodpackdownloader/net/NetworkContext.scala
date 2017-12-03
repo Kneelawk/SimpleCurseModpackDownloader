be4227eda87459c4313c337e190589bbeeba5a59
Kneelawk <jedidiah.pommert@gmail.com>
2017-12-03 03:21:50 -0800

    Add NetworkContext

---
+++ src/main/scala/org/kneelawk/simplecursemodpackdownloader/net/NetworkContext.scala
@@ -0,0 +1,29 @@
+package org.kneelawk.simplecursemodpackdownloader.net
+
+import java.io.Closeable
+
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient
+import org.apache.http.impl.nio.client.HttpAsyncClients
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder
+
+class NetworkContext(c: CloseableHttpAsyncClient) extends Closeable {
+  def client = c
+
+  def start() = c.start()
+
+  def close() = c.close()
+}
+
+object NetworkContext {
+  def apply(): NetworkContext = new NetworkContext(HttpAsyncClients.createDefault())
+
+  def apply(client: CloseableHttpAsyncClient): NetworkContext = new NetworkContext(client)
+
+  def custom(): NetworkContextBuilder = new NetworkContextBuilder(HttpAsyncClients.custom())
+}
+
+class NetworkContextBuilder(b: HttpAsyncClientBuilder) {
+  def setUserAgent(userAgent: String) = b.setUserAgent(userAgent)
+
+  def build(): NetworkContext = new NetworkContext(b.build())
+}
