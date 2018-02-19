953f93467fd005efadaa1db804ca040b6b5f3727
Kneelawk <jedidiah.pommert@gmail.com>
2018-02-18 18:39:25 -0800

    Fix TaskManifest

--- src/main/scala/org/kneelawk/simplecursemodpackdownloader/task/TaskManifest.scala
+++ src/main/scala/org/kneelawk/simplecursemodpackdownloader/task/TaskManifest.scala
@@ -6,15 +6,30 @@ import scala.collection.mutable.Set
 class TaskManifest {
   private val tasks = new HashSet[Task]
 
+  /** Add a task to the manifest.
+   *  
+   */
   def addTask(task: Task) = tasks += task
 
+  /** Remove a task from the manifest.
+   *  
+   */
   def removeTask(task: Task) = tasks -= task
 
+  /** Get a list of tasks in the manifest.
+   *  
+   */
   def getTasks: Set[Task] = tasks
 
+  /** Loop through every task in the manifest.
+   *  
+   */
   def foreach[R](f: (Task) => R) = tasks.foreach(f)
 
+  /** Remove all stopped tasks.
+   *  
+   */
   def pruneTasks() {
-    tasks.retain((t) => t.getState.isInstanceOf[StoppedEngineState])
+    tasks.retain((t) => !t.getState.isInstanceOf[StoppedEngineState])
   }
 }
