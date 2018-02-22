3390aa9a5eec3a190cc6c51d944b054647b1b179
Kneelawk <jedidiah.pommert@gmail.com>
2018-02-21 17:57:34 -0800

    Implement TaskHandler

--- src/main/scala/org/kneelawk/simplecursemodpackdownloader/task/TaskHandler.scala
+++ src/main/scala/org/kneelawk/simplecursemodpackdownloader/task/TaskHandler.scala
@@ -1,5 +1,32 @@
 package org.kneelawk.simplecursemodpackdownloader.task
 
-class TaskHandler(builder: TaskBuilder, parent: Task) {
+import scala.concurrent.ExecutionContext
+import java.util.concurrent.CountDownLatch
+import scala.concurrent.duration.Duration
+import scala.concurrent.Future
+
+class TaskHandler(task: Task)(implicit ctx: ExecutionContext) {
+  private val done = new CountDownLatch(1)
   
+  task.getBus.register((e: TaskStateChangeEvent) => {
+    if (e.state.isInstanceOf[RestartableEngineState]) {
+      start()
+    } else if (e.state.isInstanceOf[StoppedEngineState]) {
+      done.countDown()
+    }
+  })
+
+  def start() {
+    Future {
+      task.start()
+    }
+  }
+
+  def await() {
+    done.await()
+  }
+
+  def await(d: Duration) {
+    done.await(d.length, d.unit)
+  }
 }
